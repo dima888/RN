4aea155efdeb3faaf7e1ce0d5dcce22ef094a4bb
dima888 <dima888@hotmail.de>
2013-10-27 23:54:10 +0100

    oO

--- src/proxyServer/POP3_Server_Commands.java
+++ src/proxyServer/POP3_Server_Commands.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Scanner;
 
 import javax.swing.text.html.parser.Parser;
 
@@ -17,6 +18,7 @@ class POP3_Server_Commands {
 
 	 static String ok = "+OK";
 	 static String err = "-ERR";
+	 static File f = new File(ServerAccountManagement.getDirPath2().toString());
 	 
 	 //******************************** PRIVATER KONSTRUKTOR ******************************************
 	 
@@ -61,9 +63,36 @@ class POP3_Server_Commands {
 		return null;
 	}
 	 
+	  /** TODO: DAT MACHT AUCH DIMA; SCHON HIER ANGFANGEN: Hier noch auf die Punkte(. || .. || .) achten
+	   * Holt die n-te E-Mail vom E-Mail-Server
+	   * @param secondPartCommand
+	   * @return
+	   */
 	 static String retr(String secondPartCommand) {
+		 int count = 0;
+		 String result = ok + "\n";
+		 try {
+			 int integer = Integer.parseInt(secondPartCommand);
+			 
+			 for(File i : f.listFiles()) {
+				 if(++count == integer) {
+					 
+					 //Datei auslesen
+					 Scanner scanner = new Scanner(i);
+					 while(scanner.hasNext()) {
+						 result += scanner.next() + " ";
+					 }					 
+				 }
+			 }
+			 
+		} catch (Exception e) {
+			return "-ERR invalid sequence number: " + secondPartCommand;
+		}
+		 		 		
+		 
+		 
 		// TODO Auto-generated method stub
-		return null;
+		return result;
 	}
 
 	 static String noop() {
@@ -89,26 +118,26 @@ class POP3_Server_Commands {
 
 	 /**
 	  * Liefert den Status der Mailbox, u.a. die Anzahl aller E-Mails im Postfach und deren Gesamtgröße (in Byte).
-	  * @return
-	  * @throws IOException 
+	  * @return result - fertiger String mit den oben genannten infos
 	  */
 	 static String stat()  {	
+		 String result = "";
 		 int count = 0;
-		 ServerAccountManagement.getDirPath2();
-		 File f = new File(ServerAccountManagement.getDirPath2().toString());
+		 int completeLengh = 0;
 		 
-		 
-		System.out.println(f.listFiles());
+		
 		for(File i : f.listFiles()) {
+			completeLengh += i.length();
 			count ++;
 		}
 		
 		Integer c = new Integer(count);
 		String emailCount = c.toString();
-		System.out.println(emailCount);
-		 
-		// TODO Auto-generated method stub
-		return null;
+		
+		result += ok += " ";
+		result += emailCount += " ";		
+		result += completeLengh;
+		return result;
 	}
 
 	 /**
@@ -128,6 +157,8 @@ class POP3_Server_Commands {
 	
 	//**************************** TEST *****************************
 	public static void main(String[] args) {
-		stat();
+		System.out.println(retr("$"));
+		System.out.println(retr("1"));
+//		System.out.println(stat());
 	}
 }
