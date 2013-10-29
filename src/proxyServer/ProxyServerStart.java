faf93ad4339ef4a6496d54566cb4be4f56d7c84c
Flah <flah.ahmad@haw-hamburg.de>
2013-10-29 20:01:06 +0100

    commit

--- src/proxyServer/ProxyServerStart.java
+++ src/proxyServer/ProxyServerStart.java
@@ -19,7 +19,7 @@ public class ProxyServerStart {
 	 * @param SERVER_PORT - Port des Servers
 	 */
 	
-	private POP3_Server_Commands server_commands = new POP3_Server_Commands(Paths.get("C:\\Users\\dima\\Desktop\\Emails"));
+	private POP3_Server_Commands server_commands = new POP3_Server_Commands(Paths.get("C:\\Users\\Sony\\Desktop\\Emails"));
 	
 	public void starteClient(String clientName) {
 		POP3Client client = new POP3Client(clientName, server_commands);
@@ -39,10 +39,7 @@ public class ProxyServerStart {
 		//*************************************************************************************************************************************************
 		//ProxyServer erstellen
 		ProxyServerStart proxyServer = new ProxyServerStart();
-	
-		
-		//proxyServer.starteClient("foxhound");
-		
+			
 		proxyServer.starteClient("foxhound");
 		proxyServer.starteServer();
 		
