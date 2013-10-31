214ad73872c3a3c083d9d1f0da7a23ca13384123
dima888 <dima888@hotmail.de>
2013-10-31 17:48:03 +0100

    Denkst ja nur du

--- src/proxyServer/ProxyServerStart.java
+++ src/proxyServer/ProxyServerStart.java
@@ -17,7 +17,7 @@ public class ProxyServerStart {
 	private ServerAccountManagement account1 = new ServerAccountManagement("flah", "pop.gmx.de", 110, "flah_ahmad@gmx.de", "RN2013Huebner");
 	private ServerAccountManagement account2 = new ServerAccountManagement("foxhound", "pop.gmx.net", 110, "dima888@gmx.net", "12345678");
 	
-	private POP3_Server_Commands server_commands = new POP3_Server_Commands(Paths.get("C:\\Users\\abh840\\Desktop\\Emails"));
+	private POP3_Server_Commands server_commands = new POP3_Server_Commands(Paths.get("C:\\Users\\abg688\\Desktop\\Emails"));
 	
 	private void starteClient() {
 		POP3Client client = new POP3Client("foxhound", server_commands);
@@ -29,13 +29,8 @@ public class ProxyServerStart {
 	}
 	
 	public void starteProxyServer() {
+		this.server_commands.aktualisieren();		
 		this.starteClient();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		this.starteServer();
 	}
 	
